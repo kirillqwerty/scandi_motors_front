83b8a29a08fbec978836da90cffc08d9465b4bac
Тиханович Кирилл Сергеевич <kiryl.tsikhanovich@softclub.by>
2026-04-15 11:35:10 +0300

    add gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/scandi_motors_front",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
